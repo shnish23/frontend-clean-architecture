ff972f15ae0723410026f8bb36a8e1d6918fdf7f
shnish23 <shinichi.nishihata@gmail.com>
2021-09-10 08:16:42 +0000

    feat:オーダー

--- src/ui/User/User.tsx
+++ src/ui/User/User.tsx
@@ -3,6 +3,7 @@ import { Redirect } from "react-router";
 import { useUserStorage } from "../../services/storageAdapter";
 import { Buy } from "../Buy";
 import { Cart } from "../Cart";
+import { Orders } from "../Orders";
 import { Profile } from "../Profile";
 
 export function User() {
@@ -12,6 +13,7 @@ export function User() {
   return (
     <main>
       <Profile />
+      <Orders />
       <Cart />
       <Buy />
     </main>
